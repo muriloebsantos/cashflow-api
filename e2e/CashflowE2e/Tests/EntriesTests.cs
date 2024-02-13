a96b8eece417419ec05728ae22776df0b9ee735c
muriloebsantos <murilo.bacagini@gmail.com>
2024-02-13 16:34:38 -0300

    tests: add file

---
+++ e2e/CashflowE2e/Tests/EntriesTests.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CashflowE2e.Tests
+{
+    public class EntriesTests
+    {
+        // Todo: Should_Create_Single_Entry (Credit/Debit)
+        // Todo: Should_Create_Recurring_Entry (Credit/Debit) (Daily, Weekly, Monthly, Yearly)
+        // Todo: Should_Create_Single_Credit_Card_Entry (Credit/Debit)
+        // Todo: Should_Create_Recurring_Credit_Card_Entry (Credit/Debit)
+        // Todo: Should_Create_Single_Entry_As_Paid_And_Update_Balance (Credit/Debit)
+        // Todo: Should_Apply_Single_Entry_And_Update_Balance (Credit/Debit)
+        // Todo: Should_Apply_Credit_Card_Invoice_And_Update_Balance
+        // Todo: Should_Update_Single_Entry
+        // Todo: Should_Update_Recurring_Entry
+        // Todo: Should_Delete_Single_Entry 
+        // Todo: Should_Delete_Recurring_Entry
+        // Todo: Should_Delete_Credit_Card_Entry
+        // Todo: Should_List_Pending_Entries
+    }
+}
